68a5fafa8b39623076842575870199ba9f180aea
n0izn0iz <norman.n0ii.m@gmail.com>
2014-11-20 12:27:50 +0100

    color + more tests

--- run_tests.sh
+++ run_tests.sh
@@ -6,10 +6,9 @@ IFS=$'\n'
 declare -i total=0
 declare -i failed=0
 echo "STARTING TESTS" | tee results
-echo "" | tee -a results
+#echo "" | tee -a results
 for line in $(grep -v '^#' $TESTFILE)
 do
-	total=$(( total + 1))
 	echo "----> lslog$total" > logs/lslog$total
 	echo "----> ftlslog$total" > logs/ftlslog$total
 	echo "[TEST$total] \"ls $line\"" | tee -a results logs/lslog$total logs/ftlslog$total
@@ -22,18 +21,19 @@ do
 	then
 		failed=$(( failed + 1 ))
 		echo "$DIFF" >> results
-		echo "--------> [FAIL]" | tee -a results
+		echo "[\033[31mFAIL\033[0m]" | tee -a results
 	else
-		echo "--> [PASS]" | tee -a results
+		echo "[\033[032mPASS\033[0m]" | tee -a results
 	fi
-	echo "" | tee -a results
+	#echo "" | tee -a results
+	total=$(( total + 1))
 done
 if [ $failed != 0 ]
 then
-	echo "TESTING DONE, FAILED $failed TESTS ON $total TOTAL" | tee -a results
+	echo "\033[31mTESTING DONE, FAILED $failed TESTS ON $total TOTAL\033[0m" | tee -a results
 	echo "See the results file for more information about failed tests"
 else
-	echo "TESTING DONE, ALL $total TESTS PASSED" | tee -a results
+	echo "\033[32mTESTING DONE, ALL $total TESTS PASSED\033[0m" | tee -a results
 fi
 echo "See the logs files for tests output"
 IFS=$OLDIFS
